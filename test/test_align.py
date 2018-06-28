4f54beef917695095e9a249a05afc6d987568ff6
snow13bbc <snow.naing@ucsf.edu>
2018-06-29 00:32:27 +0200

    editing opt test case again

--- test/test_align.py
+++ test/test_align.py
@@ -7,19 +7,19 @@ def test_alignment():
     seq1 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
     seq2 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
     blosum62 = io.read_score_matrix('matrices/BLOSUM50')
-    aln, score, _ = smithwaterman.align(seq1, seq2, -5, -1, blosum62)
+    _, _1, score, _2 = smithwaterman.align(seq1, seq2, -5, -1, blosum62)
 
     # update this assertion
     assert score == 333.0
 
-df=pd.read_csv('matrices_mut/pam100-mut.txt', sep=' ', header=0)
-df = df.set_index('Unnamed: 0')
+
 
 def test_optimization():
-        seq1 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
-        seq2 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
-        blosum62 = io.read_score_matrix('matrices_mut/pam100-mut.txt')
-        aln, score, _ = smithwaterman.align(seq1, seq2, -5, -1, df)
+    df=pd.read_csv('matrices_mut/pam100-mut.txt', sep=' ', header=0)
+    df = df.set_index('Unnamed: 0')
+    seq1 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
+    seq2 = 'MAAAAAAGAGPEMVRGQVFDVGPRYTNLSYIGEGAYGMVCSAYDNVNKVR'
+    _, _1, score, _2 = smithwaterman.align(seq1, seq2, -5, -1, df)
 
-        # update this assertion
-        assert score == 301.0
+    # update this assertion
+    assert score == 301.0
